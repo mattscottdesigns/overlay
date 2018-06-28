56f5671ae7951dcaefe52df31f02a865f0af1e04
Matt Scott <mscott@cdsreg.com>
2018-06-27 22:54:38 -0400

    Refactor check in

--- overlay.js
+++ overlay.js
@@ -9,11 +9,10 @@
 
 	var pluginName = "overlay",
 		defaults = {
-			"selector": "fieldset",
-			"stepSize": 1,
 			"currentStep": 1,
 			"hide": undefined,
 			"remove": undefined,
+			"steps": []
 		};
 
 
@@ -24,45 +23,39 @@
 		var _this = this;
 
 		_this._defaults = $.extend({}, $.fn[pluginName].defaults, defaults);
-		_this.element = element;
 		_this.settings = $.extend({}, _this._defaults, options);
-		
-		if( _this.settings.enabled ){
+		_this.element = element.addClass("overlay-element");
+
+		if (_this.settings.enabled) {
 			_this.init();
 			return _this;
-		}else{
-			return false;
 		}
-		
+
 	}
 
 
+	
 	/**
 	 * Extend the Overlay function to add new functionality
 	 */
 	$.extend(Overlay.prototype, {
 
 
-		/**
-		 * Initialize the plugin
-		 */
 		init: function () {
 			var _this = this;
+
+			$("head").append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />')
 
 			_this.createOverlay()
 				.createProps()
 				.createSteps()
-				.bindActions()
-				.setStep();
+				.setActive()
 
 			return _this;
 		},
 
 
-		/**
-		 * Bind all actions in the plugin
-		 */
-		bindActions: function () {
+		bindActions(){
 			var _this = this;
 
 			$(".btn-back").each(function () {
@@ -74,81 +67,74 @@
 			$(".btn-next,[type='submit']").each(function () {
 				$(this).on("click", function (e) {
 					e.preventDefault();
-
-					if (_this.onValidate && typeof (_this.onValidate) === "function") {
-						if (_this.onValidate()) {
+					if (_this.settings.onValidate && typeof (_this.settings.onValidate) === "function") {
+						if (_this.settings.onValidate()) {
 							_this.moveNext();
 						}
 					} else {
 						_this.moveNext();
 					}
-
 				})
 			});
-
-			if (_this.settings.hide) {
-				$(_this.settings.hide).hide();
-			}
-
-			if (_this.settings.remove) {
-				$(_this.settings.remove).remove();
-			}
-
-			return _this;
 		},
 
 
-		/**
-		 * Create the Overlay layout in the DOM
-		 */
 		createOverlay: function () {
 			var _this = this;
-
-			$("head").append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />')
 
 			$("body")
 				.addClass("overlay")
 				.prepend("<div class='overlay-container'><div class='overlay-content' /></div>");
 
+			$(".overlay-content").append(_this.element);
+
+			_this.settings.steps.map(function (step, i) {
+				var intStep = i + 1;
+				var step = ("<div class='overlay-step step-" + intStep + "' />");
+
+				_this.element.append(step);
+
+				return step
+			});
+
 			return _this;
 		},
 
 
-		/**
-		 * Create all properties and functions for the plugin
-		 */
 		createProps: function () {
 			var _this = this;
 
-			_this.form = $(_this.element);
-			_this.overlay = $(".overlay-content").append(_this.form);
-			_this.fields = $(_this.form).find(_this.settings.selector);
-			_this.steps = [];
-			_this.numberOfSteps = Math.ceil(_this.fields.length / _this.settings.stepSize);
+			_this.container = $(".overlay-container");
+			_this.content = $(".overlay-content");
 			_this.currentStep = _this.settings.currentStep;
-			_this.onSubmit = _this.settings.onSubmit;
-			_this.onValidate = _this.settings.onValidate;
+			_this.numberOfSteps = _this.settings.steps.length;
 
 			return _this;
 		},
 
 
-		/**
-		 * Create the step progress item - indicates what step in the wizard they are on.
-		 */
-		createStepProgress: function () {
+		createSteps: function () {
 			var _this = this;
 
-			if ($(".overlay-actions").length > 0) {
-				$(".overlay-actions").remove();
+			for (var x = 0; x < _this.numberOfSteps; x++) {
+				$(".step-" + ( x + 1)).append(_this.settings.steps[x].content)
 			}
 
-			var $progress = $("<div class='overlay-actions text-center' />")
+			return _this;
+		},
+
+
+		createProgress: function () {
+			var _this = this;
+
+			$(".overlay-actions").remove();
+
+			var progress = $("<div class='overlay-actions text-center' />")
 				.append("<button type='button' class='btn-back'><i class='fa fa-angle-left fa-2x'></i></button>")
 				.append("<span class='overlay-progress text-center text-muted'>" + _this.currentStep + " / " + _this.numberOfSteps + "</span>")
 				.append("<button type='submit' class='btn-next'><i class='fa fa-angle-right fa-2x'></i></button>")
 
-			_this.overlay.append($progress)
+			$(".overlay-element").append(progress)
 
 			_this.bindActions();
 
@@ -156,58 +142,10 @@
 		},
 
 
-		/**
-		 * Create the steps that will be used for the current instance of the plugin
-		 */
-		createSteps: function () {
-			var _this = this;
-			_this.steps = [];
-
-			if (_this.settings.render) {
-
-				_this.steps = _this.settings.render.map(function (step, i) {
-					return $(step.selector)
-				})
-
-				for (var x = 0; x < _this.steps.length; x++) {
-					var step = x + 1;
-					_this.form.append("<div class='overlay-step step-" + step + "' data-step='" + step + "'/>");
-
-					$(".step-" + step)
-						.append(function () {
-							if (_this.settings.render[x].title) {
-								return "<h1 class='overlay-title'>" + _this.settings.render[x].title + "</h1>"
-							}
-						})
-						.append(_this.steps[x])
-
-				}
-
-			} else {
-
-				for (var x = 0; x < _this.numberOfSteps; x++) {
-					var step = x + 1;
-					var startSlice = _this.settings.stepSize * x;
-					var endSlice = _this.settings.stepSize * x + _this.settings.stepSize;
-
-					_this.steps.push(_this.fields.slice(startSlice, endSlice))
-					_this.form.append("<div class='overlay-step step-" + step + "' data-step='" + step + "'/>");
-
-					$(".step-" + step)
-						.append(_this.steps[x])
-				}
-			}
-
-			_this.numberOfSteps = _this.steps.length;
-			return _this;
-		},
-
-
 		moveBack: function () {
 			var _this = this;
-
 			if ((_this.currentStep - 1) > 0) {
-				_this.setStep(-1);
+				_this.setActive(-1);
 			} else {
 				window.history.back();
 			}
@@ -218,69 +156,34 @@
 			var _this = this;
 
 			if (_this.currentStep === _this.numberOfSteps) {
-				if (_this.onSubmit && typeof (_this.onSubmit) === "function") {
-					_this.onSubmit();
+				if (_this.settings.onSubmit && typeof (_this.settings.onSubmit) === "function") {
+					_this.settings.onSubmit();
 				} else {
 					_this.form.submit();
 				}
-
 			} else {
-				_this.setStep(1)
+				_this.setActive(1)
 			}
 		},
 
 
-		/**
-		 * Set the current step of the plugin - the step that needs to be shown on screen
-		 */
-		setStep: function (intStep) {
+		setActive: function (intStep) {
 			var _this = this;
-			var nextStep = intStep ? _this.currentStep + intStep : _this.settings.currentStep;
 
-			$("[data-step='" + _this.currentStep + "']").hide().removeClass("active")
-			$("[data-step='" + nextStep + "']").fadeIn().addClass("active")
+			var activeStep = intStep ? _this.currentStep + intStep : _this.currentStep;
 
-			_this.currentStep = nextStep;
+			$(".step-" + _this.currentStep).hide().removeClass("active")
+			$(".step-" + activeStep).fadeIn().addClass("active")
 
-			_this.createStepProgress()
-				.setOffset();
+			_this.currentStep = activeStep;
 
+			if( _this.settings.steps ){
+				_this.createProgress();
+			}
+			
 			return _this;
 		},
 
-
-		/**
-		 * Set the positioning of the overlay
-		 */
-		setOffset: function () {
-			var _this = this;
-
-			if (_this.settings.offsetTop) {
-				$(".overlay-step").css({
-					"marginTop": _this.settings.offsetTop()
-				})
-
-				$(window).resize(function () {
-					$(".overlay-step").css({
-						"marginTop": _this.settings.offsetTop()
-					})
-				})
-			}
-
-			if (_this.settings.offsetBottom) {
-				$(".overlay-step").css({
-					"marginBottom": _this.settings.offsetBottom()
-				})
-
-				$(window).resize(function () {
-					$(".overlay-step").css({
-						"marginBottom": _this.settings.offsetBottom()
-					})
-				})
-			}
-
-			return _this;
-		},
 	});
 
 
@@ -293,22 +196,4 @@
 	};
 
 
-
-
-
 })(jQuery, window, document);
-
-/**
- * Plugin Options
- * 
- * [selector] - string/Jquery selector     
- * 
- * [stepSize] - integer
- * 
- * [hide] - string/Jquery selector
- * 
- * [remove] - string/Jquery selector
- * 
- * [render] - array of objects
- * 
- */
